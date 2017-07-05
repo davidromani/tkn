30f505e34b607f937c8dfba8f482a34cd8dbb981
David Romaní <david@flux.cat>
2017-07-05 19:13:09 +0200

    improve ansible workshop slides 2

--- keynotes/ansible-workshop.rb
+++ keynotes/ansible-workshop.rb
@@ -1,120 +1,128 @@
-section "\e[41m   Ansible Workshop   \e[0m" do
+center <<-EOS
+\e[32;1m █████╗ ███╗   ██╗███████╗██╗██████╗ ██╗     ███████╗    ██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗███████╗██╗  ██╗ ██████╗ ██████╗ 
+██╔══██╗████╗  ██║██╔════╝██║██╔══██╗██║     ██╔════╝    ██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝██╔════╝██║  ██║██╔═══██╗██╔══██╗
+███████║██╔██╗ ██║███████╗██║██████╔╝██║     █████╗      ██║ █╗ ██║██║   ██║██████╔╝█████╔╝ ███████╗███████║██║   ██║██████╔╝
+██╔══██║██║╚██╗██║╚════██║██║██╔══██╗██║     ██╔══╝      ██║███╗██║██║   ██║██╔══██╗██╔═██╗ ╚════██║██╔══██║██║   ██║██╔═══╝ 
+██║  ██║██║ ╚████║███████║██║██████╔╝███████╗███████╗    ╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗███████║██║  ██║╚██████╔╝██║     
+╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚═╝╚═════╝ ╚══════╝╚══════╝     ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     \e[0m
+EOS
 
-  block <<-EOS
-    ┌──────────────┬────────────────┐
-    │ \e[43m   theory   \e[0m │ \e[42m   practice   \e[0m │
-    └──────────────┴────────────────┘
-  EOS
+block <<-EOS
+┌──────────────┬────────────────┐
+│ \e[43m   theory   \e[0m │ \e[42m   practice   \e[0m │
+└──────────────┴────────────────┘
+EOS
 
-  block <<-EOS
-    ┌──────────────┐
-    │ \e[43m   theory   \e[0m │
-    └──────────────┘
-
-
-    * inventory
-    * modules
-    * playbooks
-    * roles
-  EOS
-
-  block <<-EOS
-    ┌──────────────────────────┐
-    │ \e[43m   theory   \e[0m > \e[33;1minventory\e[0m │
-    └──────────────────────────┘
-    
-    
-    # simple text file with hosts description
-    
-    # default locations (sorted by priority)
-      * /etc/ansible/hosts
-      * ~/.ansible/hosts
-      * <CURRENT_LOCATION>/hosts
-
-    # default location configurable in /etc/ansible/ansible.cfg
-
-    # explicit location on every run
+block <<-EOS
+┌──────────────┐
+│ \e[43m   theory   \e[0m │
+└──────────────┘
 
 
+* inventory
+* modules
+* playbooks
+* roles
+EOS
+
+block <<-EOS
+┌──────────────────────────┐
+│ \e[43m   theory   \e[0m > \e[33;1minventory\e[0m │
+└──────────────────────────┘
 
 
-    \e[33;1mhttp://docs.ansible.com/ansible/intro_inventory.html\e[0m
-  EOS
+# simple text file with hosts description
 
-  block <<-EOS
-    ┌────────────────────────────────────┐
-    │ \e[43m   theory   \e[0m > \e[33;1minventory\e[0m > example │
-    └────────────────────────────────────┘
+# default locations (sorted by priority)
+  * /etc/ansible/hosts
+  * ~/.ansible/hosts
+  * <CURRENT_LOCATION>/hosts
 
+# default location configurable in /etc/ansible/ansible.cfg
 
-    [flux]
-    s3     ansible_host=37.187.75.127  ansible_user=root
-    s5     ansible_host=198.50.145.159 ansible_user=root
-    s6     ansible_host=37.187.172.4   ansible_user=root
-
-    [som]
-    sw3    ansible_host=164.132.200.9  ansible_user=somadmin
-    sf5    ansible_host=5.135.138.55   ansible_user=somadmin
-
-    [local]
-    mother ansible_connection=local  
-  EOS
-
-  block <<-EOS
-    ┌────────────────────────┐
-    │ \e[43m   theory   \e[0m > \e[33;1mmodules\e[0m │
-    └────────────────────────┘
-    
-    
-    # commands: script, shell...
-    
-    # files: copy, fetch, backup, synchronize...
-
-    # databases: mysql, mssql, postgresql, mongo...
-
-    # web services: apache, nginx, jenkins, jira...
-
-    # monitoring: logstash, nagios, newrelic...
-
-    # notification: mail, slack, hipchat, irc, telegram...
+# explicit location on every run
 
 
 
 
-    \e[33;1mhttp://docs.ansible.com/ansible/modules_by_category.html\e[0m
-  EOS
+\e[33;1mhttp://docs.ansible.com/ansible/intro_inventory.html\e[0m
+EOS
 
-  block <<-EOS
-    ┌──────────────────────────┐
-    │ \e[43m   theory   \e[0m > \e[33;1mplaybooks\e[0m │
-    └──────────────────────────┘
-    
-    
-    # simple yaml file with a tasks list
-    
-    # variables
-    
-    # includes
-    
-    # conditionals
-    
-    # loops
-    
-    # resuable blocks
-    
-    # templates (as a skeleton files)
+block <<-EOS
+┌────────────────────────────────────┐
+│ \e[43m   theory   \e[0m > \e[33;1minventory\e[0m > example │
+└────────────────────────────────────┘
 
 
+[flux]
+s3     ansible_host=37.187.75.127  ansible_user=root
+s5     ansible_host=198.50.145.159 ansible_user=root
+s6     ansible_host=37.187.172.4   ansible_user=root
 
-    \e[33;1mhttp://docs.ansible.com/ansible/playbooks.html\e[0m
-  EOS
+[som]
+sw3    ansible_host=164.132.200.9  ansible_user=somadmin
+sf5    ansible_host=5.135.138.55   ansible_user=somadmin
 
-  block <<-EOS
-    ┌────────────────────────────────────┐
-    │ \e[43m   theory   \e[0m > \e[33;1mplaybooks\e[0m > example │
-    └────────────────────────────────────┘
-    
-    
+[local]
+mother ansible_connection=local  
+EOS
+
+block <<-EOS
+┌────────────────────────┐
+│ \e[43m   theory   \e[0m > \e[33;1mmodules\e[0m │
+└────────────────────────┘
+
+
+# commands: script, shell...
+
+# files: copy, fetch, backup, synchronize...
+
+# databases: mysql, mssql, postgresql, mongo...
+
+# web services: apache, nginx, jenkins, jira...
+
+# monitoring: logstash, nagios, newrelic...
+
+# notification: mail, slack, hipchat, irc, telegram...
+
+
+
+
+\e[33;1mhttp://docs.ansible.com/ansible/modules_by_category.html\e[0m
+EOS
+
+block <<-EOS
+┌──────────────────────────┐
+│ \e[43m   theory   \e[0m > \e[33;1mplaybooks\e[0m │
+└──────────────────────────┘
+
+
+# simple yaml file with a tasks list
+
+# variables
+
+# includes
+
+# conditionals
+
+# loops
+
+# resuable blocks
+
+# templates (as a skeleton files)
+
+
+
+
+\e[33;1mhttp://docs.ansible.com/ansible/playbooks.html\e[0m
+EOS
+
+block <<-EOS
+┌────────────────────────────────────┐
+│ \e[43m   theory   \e[0m > \e[33;1mplaybooks\e[0m > example │
+└────────────────────────────────────┘
+
+
 - name: warm-up cache again
   shell: chdir={{ a_dir }}
     {{ a_command }} cache:warmup {{ options }}
@@ -137,60 +145,61 @@ section "\e[41m   Ansible Workshop   \e[0m" do
   service:
     state: reloaded
     name: apache2
-  EOS
+EOS
 
-  block <<-EOS
-    ┌──────────────────────┐
-    │ \e[43m   theory   \e[0m > \e[33;1mroles\e[0m │
-    └──────────────────────┘
-    
-    
-    # complex folder structure with multiple yaml files inside
-    
-    # defines a dynamic and reusable partiture (playbook)
-    
-    # community (galaxy) repository
-    
-    # easy to browse, install & play 
+block <<-EOS
+┌──────────────────────┐
+│ \e[43m   theory   \e[0m > \e[33;1mroles\e[0m │
+└──────────────────────┘
 
 
+# complex folder structure with multiple yaml files inside
 
-    \e[33;1mhttp://docs.ansible.com/ansible/galaxy.html\e[0m
-  EOS
+# defines a dynamic and reusable partiture (playbook)
 
-  block <<-EOS
-    ┌────────────────┐
-    │ \e[42m   practice   \e[0m │
-    └────────────────┘
+# community (galaxy) repository
+
+# easy to browse, install & play 
 
 
-    * installation
-    * configuration
-    * run a playbook
-    * browse galaxy
-  EOS
 
-  block <<-EOS
-    ┌───────────────────────────────┐
-    │ \e[42m   practice   \e[0m > \e[32;1minstallation\e[0m │
-    └───────────────────────────────┘
-  EOS
+\e[33;1mhttp://docs.ansible.com/ansible/galaxy.html\e[0m
+EOS
 
-  block <<-EOS
-    ┌────────────────────────────────┐
-    │ \e[42m   practice   \e[0m > \e[32;1mconfiguration\e[0m │
-    └────────────────────────────────┘
-  EOS
+block <<-EOS
+┌────────────────┐
+│ \e[42m   practice   \e[0m │
+└────────────────┘
 
-  block <<-EOS
-    ┌─────────────────────────────────┐
-    │ \e[42m   practice   \e[0m > \e[32;1mrun a playbook\e[0m │
-    └─────────────────────────────────┘
-  EOS
 
-  block <<-EOS
-    ┌────────────────────────────────┐
-    │ \e[42m   practice   \e[0m > \e[32;1mbrowse galaxy\e[0m │
-    └────────────────────────────────┘
-  EOS
-end
+* installation
+* configuration
+* run a playbook
+* browse galaxy
+EOS
+
+block <<-EOS
+┌───────────────────────────────┐
+│ \e[42m   practice   \e[0m > \e[32;1minstallation\e[0m │
+└───────────────────────────────┘
+EOS
+
+block <<-EOS
+┌────────────────────────────────┐
+│ \e[42m   practice   \e[0m > \e[32;1mconfiguration\e[0m │
+└────────────────────────────────┘
+EOS
+
+block <<-EOS
+┌─────────────────────────────────┐
+│ \e[42m   practice   \e[0m > \e[32;1mrun a playbook\e[0m │
+└─────────────────────────────────┘
+EOS
+
+block <<-EOS
+┌────────────────────────────────┐
+│ \e[42m   practice   \e[0m > \e[32;1mbrowse galaxy\e[0m │
+└────────────────────────────────┘
+EOS
+
+__END__
