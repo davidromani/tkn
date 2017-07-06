bda61317af9b7a491771439314f4a3107d69ec68
David Romaní <david@flux.cat>
2017-07-06 08:16:24 +0200

    improve ansible workshop slides 3

--- keynotes/ansible-workshop.rb
+++ keynotes/ansible-workshop.rb
@@ -40,7 +40,7 @@ block <<-EOS
 
 # default location configurable in /etc/ansible/ansible.cfg
 
-# explicit location on every run
+# explicit location on every play
 
 
 
@@ -109,7 +109,7 @@ block <<-EOS
 
 # resuable blocks
 
-# templates (as a skeleton files)
+# templates (as a skeleton file)
 
 
 
@@ -123,28 +123,122 @@ block <<-EOS
 └────────────────────────────────────┘
 
 
-- name: warm-up cache again
-  shell: chdir={{ a_dir }}
-    {{ a_command }} cache:warmup {{ options }}
-- name: copy local web assets directory to remote
-  synchronize:
-    mode: push
-    recursive: yes
-    perms: yes
-    times: yes
-    src: "{{ playbook_dir }}/../../web/assets"
-    dest: "{{ a_path }}/web"
-- name: fix web assets directory to apache ownership
-  file:
-    path: "{{ a_path }}/web/assets"
-    recurse: yes
-    follow: yes
-    owner: www-data
-    group: www-data
-- name: reload apache service
-  service:
-    state: reloaded
-    name: apache2
+---
+- name: Install required server packages for production
+  hosts: s6
+  tasks:
+    - name: check server connection
+      ping:
+    - name: initial apt-get update && upgrade
+      apt:
+        update_cache: yes
+        upgrade: yes
+        autoremove: yes
+    - name: install base packages
+      apt:
+        state: present
+        name:
+          - acl
+          - git
+          - nano
+          - curl
+          - python-software-properties
+          - mysql-server-5.7
+          - mysql-client-5.7
+          - apache2
+          - libapache2-mod-fastcgi
+          - php7.1-fpm
+    - name: add additional java 8 packages repository
+      apt_repository:
+        state: present
+        repo: "ppa:webupd8team/java"
+EOS
+
+block <<-EOS
+┌────────────────────────────────────┐
+│ \e[43m   theory   \e[0m > \e[33;1mplaybooks\e[0m > example │
+└────────────────────────────────────┘
+
+
+---
+- name: Create daily MySQL backups
+  hosts: s6
+  vars:
+    - excluded_dbs:
+      - "Database"
+      - "sys"
+      - "information_schema"
+      - "performance_schema"
+      - "mysql"
+  tasks:
+    - name: create remote backup directory
+      file:
+        path:  /var/backups/mysql
+        state: directory
+        mode:  0755
+    - name: register all remote databases name
+      shell: 'mysql -u root -p<A_PASSWD> -e "show databases;"'
+      register: dblist
+    - name: dump all remote databases
+      mysql_db:
+        state: dump
+        name: "{{ item }}"
+        login_user: "<A_USER>"
+        login_password: "<A_PASSWD>"
+        target: "/var/backups/mysql/{{ ansible_date_time.date }}-{{ item }}.sql.bz2"
+      with_items: "{{ dblist.stdout_lines | difference(excluded_dbs) }}"
+    - name: copy remote databases dump files to local
+      fetch:
+        src: "/var/backups/mysql/{{ ansible_date_time.date }}-{{ item }}.sql.bz2"
+        dest: "/<A_DEST_PATH>"
+        flat: yes
+      with_items: "{{ dblist.stdout_lines | difference(excluded_dbs) }}" 
+    - name: remove remote databases dump files
+      file:
+        state: absent
+        path: "/var/backups/mysql/{{ ansible_date_time.date }}-{{ item }}.sql.bz2"
+      with_items: "{{ dblist.stdout_lines | difference(excluded_dbs) }}"
+EOS
+
+block <<-EOS
+┌────────────────────────────────────┐
+│ \e[43m   theory   \e[0m > \e[33;1mplaybooks\e[0m > example │
+└────────────────────────────────────┘
+
+
+---
+- name: Create daily web uploads backups
+  hosts: s6
+  vars:
+    - excluded_dirs:
+      - "html"
+      - "<AN_EXCLUDED_DIR>"
+  tasks:
+    - name: create remote backup directory
+      file:
+        path: /var/backups/uploads
+        state: directory
+        mode: 0755
+    - name: register all remote upload directories name
+      shell: "ls /var/www/"
+      register: dirlist
+    - name: backup all remote upload directories
+      archive:
+        path: "/var/www/{{ item }}"
+        dest: "/var/backups/uploads/{{ ansible_date_time.date }}-{{ item }}.tar.bz2"
+        format: bz2
+      with_items: "{{ dirlist.stdout_lines | difference(excluded_dirs) }}"
+    - name: copy remote upload directories to local
+      fetch:
+        src: "/var/backups/uploads/{{ ansible_date_time.date }}-{{ item }}.tar.bz2"
+        dest: "/<A_DEST_PATH>"
+        flat: yes
+      with_items: "{{ dirlist.stdout_lines | difference(excluded_dirs) }}" 
+    - name: remove remote upload directories archive files
+      file:
+        state: absent
+        path: "/var/backups/uploads/{{ ansible_date_time.date }}-{{ item }}.sql.bz2"
+      with_items: "{{ dblist.stdout_lines | difference(excluded_dbs) }}"
 EOS
 
 block <<-EOS
@@ -163,6 +257,7 @@ block <<-EOS
 
 
 
+
 \e[33;1mhttp://docs.ansible.com/ansible/galaxy.html\e[0m
 EOS
 
@@ -178,28 +273,48 @@ block <<-EOS
 * browse galaxy
 EOS
 
-block <<-EOS
+center <<-EOS
 ┌───────────────────────────────┐
 │ \e[42m   practice   \e[0m > \e[32;1minstallation\e[0m │
 └───────────────────────────────┘
+
+
+
+
+\e[33;1mhttp://docs.ansible.com/ansible/intro_installation.html\e[0m
 EOS
 
 block <<-EOS
 ┌────────────────────────────────┐
 │ \e[42m   practice   \e[0m > \e[32;1mconfiguration\e[0m │
 └────────────────────────────────┘
+
+
+$ ansible localhost -a "/bin/echo hello world"
+
+$ ansible -i hosts all -m ping
 EOS
 
-block <<-EOS
+center <<-EOS
 ┌─────────────────────────────────┐
 │ \e[42m   practice   \e[0m > \e[32;1mrun a playbook\e[0m │
 └─────────────────────────────────┘
+
+
+
+
+\e[33;1mhttps://github.com/Flexible-User-Experience/Espai-Kowo/tree/master/app/ansible\e[0m
 EOS
 
-block <<-EOS
+center <<-EOS
 ┌────────────────────────────────┐
 │ \e[42m   practice   \e[0m > \e[32;1mbrowse galaxy\e[0m │
 └────────────────────────────────┘
+
+
+
+
+\e[33;1mhttps://galaxy.ansible.com\e[0m
 EOS
 
 __END__
